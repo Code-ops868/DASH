81bd065b687abc796640628b91f56631d7d1f196
Abrao <114954398+Code-ops868@users.noreply.github.com>
2023-05-25 09:32:16 -0300

    estatistica.py

--- estatistica.py
+++ estatistica.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as xp
 import openpyxl as op
 
-st.set_page_config(page_title="DashBoard de Vendas",page_icon=":bar_chart:",layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="DashBoard de Vendas",page_icon=":bar_chart:",layout="wide", initial_sidebar_state="expanded", layout="wide")
 st.title(":green[DashBoard de Vendas]")
 st.markdown("---")
 df =pd.read_excel(
@@ -40,7 +40,7 @@ profit_by_category= (selection_query.groupby(by=['CATEGORIA']).sum(numeric_only=
 profit_by_category_barchart = xp.bar(profit_by_category,x="LUCRO",y=profit_by_category.index, title="Lucro por Categoria", color_discrete_sequence=["#17f50c"],)
 profit_by_category_barchart.update_layout(plot_bgcolor=("rgba(0,1,0,0)"),xaxis=(dict(showgrid=False)))
 
-profit_by_category_piechart = xp.pie(profit_by_category,names=profit_by_category.index,values="LUCRO",title="Media Percentual %",hole=.3, color=profit_by_category.index,
+profit_by_category_piechart = xp.pie(profit_by_category,names=profit_by_category.index,values="LUCRO",title="Media Percentual %",hole=.5, color=profit_by_category.index,
                   color_discrete_sequence=xp.colors.sequential.RdPu_r)
 coluna1, coluna2= st.columns(2)
 coluna1.plotly_chart(profit_by_category_barchart , use_container_width=True)
